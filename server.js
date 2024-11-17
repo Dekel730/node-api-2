1ccbbe45978fc1bce9ea7d0d25d1108979d3c320
Dekel Vaknin <dekelarlo@gmail.com>
2024-11-17 19:12:00 +0200

    project structure

---
+++ server.js
@@ -0,0 +1,24 @@
+import express  from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import { errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
+
+app.use(errorHandler);
