31b47866f9cdfd7d80ac36de1e686a312043c43c
Omer Gaiznger <Omerg863@gmail.com>
2024-11-18 12:19:44 +0200

    post routes

---
+++ routes/postRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createPost, getPostByUser, updatePost, getAllPosts, getPostById, deletePost } from '../controllers/postController.js';
+import { authUser } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').post(authUser, createPost);
+router.route('/').get(getPostByUser);
+router.route('/all').get(getAllPosts);
+router.route('/:id').put(authUser, updatePost);
+router.route('/:id').get(getPostById);
+router.route('/:id').delete(authUser, deletePost);
+
+export default router
