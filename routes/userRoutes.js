66b8acfe02f116886ec80274c003f8a7441a07a8
Omer Gaiznger <Omerg863@gmail.com>
2024-11-21 13:12:56 +0200

    user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { register, updateUser, deleteUser } from "../controllers/userController.js";
+import { authUser } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/").put(authUser, updateUser);
+router.route('/').delete(authUser, deleteUser);
 
 export default router;
